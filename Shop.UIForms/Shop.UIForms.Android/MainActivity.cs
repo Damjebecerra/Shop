ce7635eac7d825e1a9e8610bf6a0b75ba0a49034
Jeisson damian becerra rodriguez <jeisson_becerra45@hotmail.com>
2019-11-19 23:36:31 -0500

    Send Image to BackEnd

--- Shop.UIForms/Shop.UIForms.Android/MainActivity.cs
+++ Shop.UIForms/Shop.UIForms.Android/MainActivity.cs
@@ -6,10 +6,12 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
 
 namespace Shop.UIForms.Droid
 {
-    [Activity(Label = "Shop.UIForms", 
+    [Activity(Label = "Shop", 
         Icon = "@mipmap/ic_launcher", 
         Theme = "@style/MainTheme", 
         MainLauncher = false, 
@@ -22,16 +24,22 @@ namespace Shop.UIForms.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(
+            int requestCode,
+            string[] permissions,
+            [GeneratedEnum] Permission[] grantResults)
         {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults);
         }
+
     }
 }
--- Shop.UIForms/Shop.UIForms/ViewModels/AddProductViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/AddProductViewModel.cs
@@ -4,6 +4,9 @@
     using Common.Models;
     using Common.Services;
     using GalaSoft.MvvmLight.Command;
+    using Plugin.Media;
+    using Plugin.Media.Abstractions;
+    using Shop.Common.Helpers;
     using Xamarin.Forms;
 
     public class AddProductViewModel : BaseViewModel
@@ -11,6 +14,15 @@
         private bool isRunning;
         private bool isEnabled;
         private readonly ApiService apiService;
+        private ImageSource imageSource;
+        private MediaFile file;
+
+        public ImageSource ImageSource
+        {
+            get => this.imageSource;
+            set => this.SetValue(ref this.imageSource, value);
+        }
+
 
         public string Image { get; set; }
 
@@ -32,10 +44,13 @@
 
         public ICommand SaveCommand => new RelayCommand(this.Save);
 
+        public ICommand ChangeImageCommand => new RelayCommand(this.ChangeImage);
+
+
         public AddProductViewModel()
         {
             this.apiService = new ApiService();
-            this.Image = "noImage";
+            this.ImageSource = "noImage";
             this.IsEnabled = true;
         }
 
@@ -43,7 +58,8 @@
         {
             if (string.IsNullOrEmpty(this.Name))
             {
-                await Application.Current.MainPage.DisplayAlert("Error", 
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", 
                     "You must enter a product name.", 
                     "Accept");
                 return;
@@ -51,7 +67,8 @@
 
             if (string.IsNullOrEmpty(this.Price))
             {
-                await Application.Current.MainPage.DisplayAlert("Error", 
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", 
                     "You must enter a product price.", 
                     "Accept");
                 return;
@@ -69,15 +86,20 @@
             this.IsRunning = true;
             this.IsEnabled = false;
 
-            //TODO: Add image
+            byte[] imageArray = null;
+            if (this.file != null)
+            {
+                imageArray = FilesHelper.ReadFully(this.file.GetStream());
+            }
+
             var product = new Product
             {
                 IsAvailabe = true,
                 Name = this.Name,
                 Price = price,
                 User = new User { UserName = MainViewModel.GetInstance().UserEmail,
-                    Email = MainViewModel.GetInstance().UserEmail
-                }
+                    Email = MainViewModel.GetInstance().UserEmail},
+                ImageArray = imageArray
             };
 
             var url = Application.Current.Resources["UrlAPI"].ToString();
@@ -104,5 +126,49 @@
             this.IsEnabled = true;
             await App.Navigator.PopAsync();
         }
+
+        private async void ChangeImage()
+        {
+            await CrossMedia.Current.Initialize();
+
+            var source = await Application.Current.MainPage.DisplayActionSheet(
+                "Where do you take the picture?",
+                "Cancel",
+                null,
+                "From Gallery",
+                "From Camera");
+
+            if (source == "Cancel")
+            {
+                this.file = null;
+                return;
+            }
+
+            if (source == "From Camera")
+            {
+                this.file = await CrossMedia.Current.TakePhotoAsync(
+                    new StoreCameraMediaOptions
+                    {
+                        Directory = "Pictures",
+                        Name = "test.jpg",
+                        PhotoSize = PhotoSize.Small,
+                    }
+                );
+            }
+            else
+            {
+                this.file = await CrossMedia.Current.PickPhotoAsync();
+            }
+
+            if (this.file != null)
+            {
+                this.ImageSource = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+            }
+        }
+
     }
 }
